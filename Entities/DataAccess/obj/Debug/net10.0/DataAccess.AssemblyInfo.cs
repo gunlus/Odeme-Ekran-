74803b22039805186bdaeb9c8da3b1f97056e905
gunluss <gnluss@hotmail.com>
2026-09-07 10:02:12 +0300

    Entegrasyon testlerinde düzeltmeler / Some changes in entegration tests

--- Entities/DataAccess/obj/Debug/net10.0/DataAccess.AssemblyInfo.cs
+++ Entities/DataAccess/obj/Debug/net10.0/DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c49959d0772560bb332d6ac64182a66d29d0e19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaeb00d4544b871038497df23db15ea139d17c3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
